821ce1c8514d715089f310e5b7e1ee429708e064
hey-chan <liumatthew8@gmail.com>
2021-11-29 12:24:50 +1100

    added keys

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// Setup .env variables
+require('dotenv').config()
+
+console.log(`Env variable message was:\n ${process.env.NICE_MESSAGE}`)
+
 const express = require('express')
 
 // Instance. This is our server
